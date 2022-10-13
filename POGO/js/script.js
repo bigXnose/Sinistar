5199350467d54d9e38a8b13a1508e4728b6fe05d
bigXnose <93896603+bigXnose@users.noreply.github.com>
2022-10-12 19:32:22 -0600

    Changed the navigation

--- POGO/js/script.js
+++ POGO/js/script.js
@@ -1,1 +1,10 @@
 //Javascript
+function myFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+  
