f5ced2c31cd847f95c7496fa8e8c62390d3ded09
bigxnose <ian.m.mills5@gmail.com>
2022-10-18 12:56:19 -0600

    Inventory-main set up and additions to the strings

--- POGO/sub/inventory-main/js/script.js
+++ POGO/sub/inventory-main/js/script.js
@@ -1,10 +1,17 @@
 //Javascript
 function myFunction() {
-    var x = document.getElementById("myLinks");
-    if (x.style.display === "block") {
-      x.style.display = "none";
-    } else {
-      x.style.display = "block";
-    }
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
   }
-  
+}
+
+function copyToClipboard(element) {
+  var $temp = $("<input>");
+  $("body").append($temp);
+  $temp.val($(element).text()).select();
+  document.execCommand("copy");
+  $temp.remove();
+}
